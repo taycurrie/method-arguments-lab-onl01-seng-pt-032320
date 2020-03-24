7a1f3efdb01ae27cc23da0d7fd36e7547ac2b493
sunny-tier-6236 <taycurrie@gmail.com>
2020-03-24 18:42:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}").to_stdout
 end
